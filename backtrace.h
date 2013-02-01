a562a3121328cd83be61138e893276b5265e8a5a
zzmfish <zzmfish@gmail.com>
2013-02-01 18:00:03 +0800

    add "extern" modifier when include in c++

--- backtrace.h
+++ backtrace.h
@@ -1,4 +1,16 @@
+#ifndef BACKTRACE_H
+#define BACKTRACE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void backtrace();
 void backtrace_init(const char *name, int max_frame_depth, int max_frame_size);
 void backtrace_dump(const char *filepath);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
